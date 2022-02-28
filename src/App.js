abe531a72fb19266660002d0e870531033cbc986
Emily Perkins <emilyjperkinss@Emilys-MacBook-Pro.local>
2022-02-28 16:53:01 -0700

    started up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
+import React from "react";
+import {
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+
+// components
+import Home from "./components/Home";
+import Adopt from "./components/Adopt";
+import Info from "./components/Info";
+import Litters from "./components/Litters";
+import Parents from "./components/Parents";
+
 import './App.css';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/adopt">Adopt</Link>
+          </li>
+          <li>
+            <Link to="/info">More Info</Link>
+          </li>
+          <li>
+            <Link to="/litters">Litters</Link>
+          </li>
+          <li>
+            <Link to="/parents">Parents</Link>
+          </li>
+        </ul>
+      </div>
+      <Routes>
+        <Route path="/adopt">
+          <Adopt />
+        </Route>
+        <Route path="/info">
+          <Info />
+        </Route>
+        <Route path="/litters">
+          <Litters />
+        </Route>
+        <Route path="/parents">
+          <Parents />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Routes>
+    </>
   );
 }
 
-export default App;
