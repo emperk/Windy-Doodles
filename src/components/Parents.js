e123f9f74e2aa26387d9d2a17f2602e4064b31f5
Emily Perkins <emilyjperkinss@Emilys-MacBook-Pro.local>
2022-02-28 21:51:12 -0700

    wip

--- src/components/Parents.js
+++ src/components/Parents.js
@@ -1,9 +1,23 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Parents() {
   return (
     <div className="comp-container">
-      <h1>Meet the Parents</h1>
+      <header>
+        <div id="header-title">
+          <h1>Meet the Parents</h1>
+        </div>
+          <nav>
+            <div className="nav-links">
+              <Link to="/" className="indiv-link">Home</Link>
+              <Link to="/adopt" className="indiv-link">Adopt</Link>
+              <Link to="/info" className="indiv-link">More Info</Link>
+              <Link to="/litters" className="indiv-link">Litters</Link>
+              <Link to="/parents" className="indiv-link">Parents</Link>
+            </div>
+          </nav>
+      </header>
       <p>Doggo ipsum ruff doggorino very good spot ruff very taste wow, pats clouds. Ur givin me a spook corgo you are doing me a frighten pats, pupper thicc borkf bork, vvv most angery pupper I have ever seen. What a nice floof thicc clouds extremely cuuuuuute heckin angery woofer doggo, long water shoob you are doing me the shock long bois. Shibe doge smol doggo, length boy fat boi. Snoot smol heckin wow very biscit borking doggo big ol pupper, blop he made many woofs very jealous pupper.</p>
     </div>
   )
