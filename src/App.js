127bf8668fb10871b7f43207660951626cd988a8
Emily Perkins <emilyjperkinss@Emilys-MacBook-Pro.local>
2022-02-28 20:56:02 -0700

    finally rendering

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React from "react";
 import {
   Routes,
-  Route,
-  Link
+  Route
 } from "react-router-dom";
 
 // components
@@ -16,54 +15,14 @@ import "./App.css";
 
 export default function App() {
   return (
-    <>
-      <div className="App">
-          <h1>Windy's Doodles</h1>
-          <nav>
-            <div className="nav-links">
-              <Link to="/" className="indiv-link">Home</Link>
-              <Link to="/adopt" className="indiv-link">Adopt</Link>
-              <Link to="/info" className="indiv-link">More Info</Link>
-              <Link to="/litters" className="indiv-link">Litters</Link>
-              <Link to="/parents" className="indiv-link">Parents</Link>
-            </div>
-          </nav>
-        {/* <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/adopt">Adopt</Link>
-          </li>
-          <li>
-            <Link to="/info">More Info</Link>
-          </li>
-          <li>
-            <Link to="/litters">Litters</Link>
-          </li>
-          <li>
-            <Link to="/parents">Parents</Link>
-          </li>
-        </ul> */}
-      </div>
+    <div className="App">
       <Routes>
-        <Route path="/adopt">
-          <Adopt />
-        </Route>
-        <Route path="/info">
-          <Info />
-        </Route>
-        <Route path="/litters">
-          <Litters />
-        </Route>
-        <Route path="/parents">
-          <Parents />
-        </Route>
-        <Route path="/">
-          <Home />
-        </Route>
+        <Route path="/" element={ <Home /> } />
+        <Route path="/parents" element={ <Parents /> } />
+        <Route path="/litters" element={ <Litters /> } />
+        <Route path="/info" element={ <Info /> } />
+        <Route path="/adopt" element={ <Adopt /> } />
       </Routes>
-    </>
+    </div>
   );
 }
-
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,23 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Home() {
   return (
-    <>
     <div className="comp-container">
-      <h1>Home Overview</h1>
+      <h1>Windy's Doodles</h1>
+        <div className="nav-links">
+          <Link to="/" className="indiv-link">Home</Link>
+          <Link to="/adopt" className="indiv-link">Adopt</Link>
+          <Link to="/info" className="indiv-link">More Info</Link>
+          <Link to="/litters" className="indiv-link">Litters</Link>
+          <Link to="/parents" className="indiv-link">Parents</Link>
+        </div>
+      <img 
+        className="home-img"
+        src="https://www.istockphoto.com/photo/woman-with-his-golden-labradoodle-dog-reading-at-home-gm1147997709-309879049?utm_source=unsplash&utm_medium=affiliate&utm_campaign=srp_photos_top&utm_content=https%3A%2F%2Funsplash.com%2Fs%2Fphotos%2Fgoldendoodles&utm_term=goldendoodles%3A%3A%3A"
+        alt="doodle"
+      />
       <p>Doggo ipsum ruff doggorino very good spot ruff very taste wow, pats clouds. Ur givin me a spook corgo you are doing me a frighten pats, pupper thicc borkf bork, vvv most angery pupper I have ever seen. What a nice floof thicc clouds extremely cuuuuuute heckin angery woofer doggo, long water shoob you are doing me the shock long bois. Shibe doge smol doggo, length boy fat boi. Snoot smol heckin wow very biscit borking doggo big ol pupper, blop he made many woofs very jealous pupper.</p>
     </div>
-  </>
   )
 }
