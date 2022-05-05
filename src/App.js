c708089fd46a0d5e07acd59036b9948151c8dab7
Emily Perkins <emilyjperkinss@Emilys-MacBook-Pro.local>
2022-05-04 20:44:11 -0700

    wip

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import Info from "./components/Info";
 import Litters from "./components/Litters";
 import Parents from "./components/Parents";
 
-import "./App.css";
+import "./App.less";
 
 export default function App() {
   return (
