67d18661568f5457b5262c9c60520fba0e0788b5
Emily Perkins <emilyjperkinss@Emilys-MacBook-Pro.local>
2022-05-04 21:12:36 -0700

    wip

--- src/components/Home.js
+++ src/components/Home.js
@@ -60,11 +60,11 @@ export default function Home() {
             </Menu.Item>
           </Menu>
         </header>
-      </div>
-      <div className="top-container">
-        <p>
-          Doggo ipsum ruff doggorino very good spot ruff very taste wow, pats clouds. Ur givin me a spook corgo you are doing me a frighten pats, pupper thicc borkf bork, vvv most angery pupper I have ever seen. What a nice floof thicc clouds extremely cuuuuuute heckin angery woofer doggo, long water shoob you are doing me the shock long bois. Shibe doge smol doggo, length boy fat boi. Snoot smol heckin wow very biscit borking doggo big ol pupper, blop he made many woofs very jealous pupper.
-        </p>
+        <div className="top-container">
+          <p>
+            Doggo ipsum ruff doggorino very good spot ruff very taste wow, pats clouds. Ur givin me a spook corgo you are doing me a frighten pats, pupper thicc borkf bork, vvv most angery pupper I have ever seen. What a nice floof thicc clouds extremely cuuuuuute heckin angery woofer doggo, long water shoob you are doing me the shock long bois. Shibe doge smol doggo, length boy fat boi. Snoot smol heckin wow very biscit borking doggo big ol pupper, blop he made many woofs very jealous pupper.
+          </p>
+        </div>
       </div>
     </>
   );
