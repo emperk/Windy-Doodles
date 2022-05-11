85d23308d9eafbdcc34b9abc40986a9df15248b9
Emily Perkins <emilyjperkinss@Emilys-MacBook-Pro.local>
2022-05-11 15:07:48 -0700

    Changed the top-container on the Home page with more ant design.

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Menu, Typography } from 'antd';
 import { Link } from "react-router-dom";
 
-const { Title } = Typography;
+const { Title, Paragraph } = Typography;
 
 export default function Home() {
   return (
@@ -41,9 +41,10 @@ export default function Home() {
           </Menu>
         </header>
         <div className="top-container">
-          <p>
-            HOME PAGE. Doggo ipsum ruff doggorino very good spot ruff very taste wow, pats clouds. Ur givin me a spook corgo you are doing me a frighten pats, pupper thicc borkf bork, vvv most angery pupper I have ever seen. What a nice floof thicc clouds extremely cuuuuuute heckin angery woofer doggo, long water shoob you are doing me the shock long bois. Shibe doge smol doggo, length boy fat boi. Snoot smol heckin wow very biscit borking doggo big ol pupper, blop he made many woofs very jealous pupper.
-          </p>
+          <Title level={2}>Family Doodles Raised with Love</Title>
+          <Paragraph>
+            In this text box here, I'll have information regarding Windy, her business, and how her business started.
+          </Paragraph>
         </div>
       </div>
     </>
