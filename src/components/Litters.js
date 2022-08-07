15b0e818ad06932305bef3754366c1544d402bd8
Emily Perkins <emilyjperkinss@Emilys-MacBook-Pro.local>
2022-08-06 21:01:44 -0700

    rough draft for litters tab

--- src/components/Litters.js
+++ src/components/Litters.js
@@ -1,6 +1,12 @@
 import React from "react";
 import { Menu } from 'antd';
 import { Link } from "react-router-dom";
+import BlitzenImg from "../images/blitzen.jpeg";
+import CharlieImg from "../images/charlie.jpeg";
+import CometImg from "../images/comet.webp";
+import ElsaImg from "../images/elsa.jpeg";
+import GingerImg from "../images/ginger.jpeg";
+import HollyImg from "../images/holly.jpeg";
 
 export default function Litters() {
   return (
@@ -40,9 +46,38 @@ export default function Litters() {
         </header>
         <div className="top-container">
           <h2>My Litters</h2>
-          <p>
-            This page will have information about each puppy in each litter and whether or not they've been adopted.
-          </p>
+          <div className="litter-divs">
+            <div id="blitzen-div" className="indiv-litter">
+              <h3>Blitzen</h3>
+              <img src={BlitzenImg} id="blitzen-img" alt="portrait of blitzen" />
+              <p>assorted info about blitzen</p>
+            </div>
+            <div id="charlie-div" className="indiv-litter">
+              <h3>Charlie Brown</h3>
+              <img src={CharlieImg} id="charlie-img" alt="portrait of charlie" />
+              <p>assorted info about charlie</p>
+            </div>
+            <div id="comet-div" className="indiv-litter">
+              <h3>Comet</h3>
+              <img src={CometImg} id="comet-img" alt="portrait of comet" />
+              <p>assorted info about comet</p>
+            </div>
+            <div id="elsa-div" className="indiv-litter">
+              <h3>Elsa</h3>
+              <img src={ElsaImg} id="elsa-img" alt="portrait of elsa" />
+              <p>assorted info about elsa</p>
+            </div>
+            <div id="ginger-div" className="indiv-litter">
+              <h3>Ginger</h3>
+              <img src={GingerImg} id="ginger-img" alt="portrait of ginger" />
+              <p>assorted info about ginger</p>
+            </div>
+            <div id="holly-div" className="indiv-litter">
+              <h3>Holly</h3>
+              <img src={HollyImg} id="holly-img" alt="portrait of holly" />
+              <p>assorted info about holly</p>
+            </div>
+          </div>
         </div>
       </div>
     </>
